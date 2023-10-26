2415eaf5567ae14fd64054c869c8aa82935dfefb
Kami G <kam.gor45@gmail.com>
2023-10-26 22:38:05 +0200

    feat: deleted not important class

--- src/main/java/com/team/dto/StatusDTOConverter.java
+++
@@ -1,9 +0,0 @@
-package com.team.dto;
-
-import com.team.model.Status;
-
-public class StatusDTOConverter {
-    public static Status stringToStatusConverter(String statusString) {
-        return Status.NONE;
-    }
-}
